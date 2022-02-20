a71d2a12fc193b558e8f88ea814a58a26ef902b5
Asuna <95048094+AsunaAbi@users.noreply.github.com>
2022-02-20 15:43:07 +0100

    Add files via upload

--- start.bat
+++ start.bat
@@ -57,22 +57,32 @@ echo                                      █████╗   ██║    ╚�
 echo                                      ██╔══╝   ██║     ╚██╔╝      ██║    ██╔══╝   ██╔══██║ ██║╚██╔╝██║
 echo                                      ██║      ███████╗ ██║       ██║    ███████╗ ██║  ██║ ██║ ╚═╝ ██║
 echo                                      ╚═╝      ╚══════╝ ╚═╝       ╚═╝    ╚══════╝ ╚═╝  ╚═╝ ╚═╝     ╚═╝   
-echo.
+echo                      [35m-when 1 didnt work, try "python main.py or python3 main.py"[0m    
 echo.
 echo         [[36m1[0m] [31mServer-Cloner[0m                                   
-echo         [[36m2[0m] [31mYoutube[0m 
+echo         [[36m2[0m] [31mServer_Cloner v2[0m 
 echo         [[36m3[0m] [31mTiktok[0m 
 echo         [[36m4[0m] [31mDiscord[0m 
+echo         [[36m5[0m] [31mYoutube[0m
 
 set /p key=key : 
 if %key%==1 goto Server-Cloner
-if %key%==2 goto Youtube
+if %key%==2 goto v2
 if %key%==3 goto Tiktok
 if %key%==4 goto Discord
+if %key%==5 goto Youtube
 
 :Server-Cloner
 cls
 start python main.py
+start https://discord.gg/flyteam
+cls
+goto MENU
+
+
+:v2
+cls
+start python3 main.py
 start https://discord.gg/flyteam
 cls
 goto MENU
